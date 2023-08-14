17a49550bc94da292a97ef258437c37c188fc65a
Dawid Tracz <dawid.b.tracz@gmail.com>
2023-08-14 20:13:06 +0200

    add qrcode library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use qrcode::QrCode;
+
 fn main() {
-    println!("Hello, world!");
+    let code = QrCode::new(
+        b"012adkrdgrjkhdrlhglh3452adkrdg\
+          rjkhdrlhglh3452adkrdgrjkhdrldd\
+          rjkhdrlhglh3452adkrdgrjkhdrldd"
+    ).unwrap();
+
+    let string = code.render()
+        .light_color('#')
+        .dark_color(' ')
+        .build();
+    println!("{}", string);
 }
